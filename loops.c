41b6d8fa29e67420d87ef5e798e50bea7302e1cc
Manupreet555 <manpreet21106@gmail.com>
2025-07-15 14:38:46 +0530

    add nested loops

--- loops.c
+++ loops.c
@@ -5,18 +5,28 @@ int main(){
     //     printf(" Hello world\n");
     // }
 // bugg fix 
+
 // for (int i = 0; i >= 0; i++) {
 //     printf("%d\n", i);
-
 // }
-// bug fix 
+
+// // bug fix 
+
 // int i;
 // for (i=0;i< 5; i++) {
 //     printf("%d\n", i);
 // }
-// fix bug 
-for (int i = 1; i<=11; i++) {
-    if (i == 11)
-        printf("Reached 11 ");
+// // fix bug 
+// for (int i = 1; i<=11; i++) {
+//     if (i == 11)
+//         printf("Reached 11 ");
+// }
+// nested loops 
+int a,b ;
+for(a=0 ; a<7; a++){
+    printf("hello\n");
+    for (b=0 ; b<5 ;b++){
+        printf("world\n");
+    }
 }
 }
