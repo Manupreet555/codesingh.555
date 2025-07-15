c5bc8330344b04ace5d74bdfe6c80ff12eecca9c
Manupreet555 <manpreet21106@gmail.com>
2025-07-15 13:49:19 +0530

    fix bug

--- loops.c
+++ loops.c
@@ -1,7 +1,22 @@
 #include<stdio.h>
 int main(){
-    int  a ;
-    for(a=0;a<8;a++){
-        printf(" Hello world\n");
-    }
+    // int  a ;
+    // for(a=0;a<8;a++){
+    //     printf(" Hello world\n");
+    // }
+// bugg fix 
+// for (int i = 0; i >= 0; i++) {
+//     printf("%d\n", i);
+
+// }
+// bug fix 
+// int i;
+// for (i=0;i< 5; i++) {
+//     printf("%d\n", i);
+// }
+// fix bug 
+for (int i = 1; i<=11; i++) {
+    if (i == 11)
+        printf("Reached 11 ");
+}
 }
