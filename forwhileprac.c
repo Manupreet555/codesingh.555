953b64f676302de9f1b680a40174bde466a2b874
Manupreet555 <manpreet21106@gmail.com>
2025-07-18 02:17:51 +0530

    whileloopques

--- forwhileprac.c
+++ forwhileprac.c
@@ -1,13 +1,62 @@
 #include<stdio.h>
+# include<stdlib.h>
 int main (){
-    int i = 1;
-    while(i<3){
-           printf("hello\n");
-           i++;
+    // int i = 1;
+    // while(i<3){
+    //        printf("hello\n");
+    //        i++;
+    // }
+    
+    // int s =1 ;
+    // while (s!=8){
+    //     printf("hello\n");
+    //     s++;
+    // }
+
+        // SWITCH CASE 
+    while(1){
+        int ch ,x ,y,z;
+       printf(" press 1 for Addition\n ");
+       printf(" press 2 for subtraction\n ");
+       printf(" press 3 for Multiplication\n ");
+       printf(" press 4 Exit !! ");
+    scanf ("%d",&ch); 
+    switch (ch)
+    {
+    case 1 :
+      {
+        printf(" Enter 2 number ");
+        scanf("%d %d ",&x,&y);
+        z =x+y;
+        printf("\nthe addition is :- %d",z);
+        break;
+      }  
+      
+    case 2 :
+      {
+        printf(" Enter 2 number ");
+        scanf("%d %d ",&x,&z);
+        y =x-z;
+        printf("\nthe Subtraction is :- %d",y);
+        break;
+      }  
+
+    case 3 :
+      {
+        printf(" Enter 2 number ");
+        scanf("%d %d ",&x,&z);
+        y =x*z;
+        printf("\nthe Multiplication  is :- %d",y);
+        break;
+      } 
+    case 4 : 
+      {
+        exit (0);
+      }
+    default:
+      printf("Wrong in put ");
     }
-    int s =1 ;
-    while (s!=8){
-        printf("hello ");
-        s++;
     }
+
+    
 }
--- new.c
+++ new.c
@@ -1,4 +1,61 @@
 #include<stdio.h>
+#include<stdlib.h>
+int add( int x,int  y,int z ){
+            printf(" Enter 2 Number ");
+            scanf("%d %d", &x,&y);
+            z=x+y;
+            printf(" The Addition is :- %d",z);
+          return z ;
+}
+int sub(int x,int  y,int z){
+            printf(" Enter 2 Number ");
+            scanf("%d %d", &x,&y);
+            z=x-y;
+            printf(" The Subtraction  is :- %d",z);
+              return z;
+}
+int mul(int x,int  y,int z){
+            printf(" Enter 2 Number ");
+            scanf("%d %d", &x,&y);
+            z=x*y;
+            printf(" The Multiply is :- %d",z);
+            return z;
+}
 int main(){
-printf("Hello world");
+    int ch,x,y,z;
+    printf(" \nPress 1 for Add ");
+    printf(" \nPress 2 for Sub ");
+    printf(" \nPress 3 for Multiply ");
+     printf("\n  enter number 4 for exit");
+    while(1){
+        printf("\n \nEnter any above :-");
+       
+    scanf("%d",&ch);
+    switch(ch){
+        case 1 :
+        {
+            add(x,y,z);
+            break;
+        }
+          case 2 :
+        {
+            sub(x,y,z);
+            break;
+        }
+          case 3:
+        {
+            mul(x,y,z);
+            break;
+        }
+        case 4:
+        {
+           exit (0);
+        }
+        default :
+        {
+            printf(" Wrong Num");
+        }
+    }
+    }
+    
 }
